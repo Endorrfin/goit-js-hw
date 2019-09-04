b7b007b006f2c0f5d00e4271f2b546ccf0d62843
Vasyl Krupka <krupka.ua@gmail.com>
2019-09-04 08:58:52 +0300

    GoIT BootCamp#14 course js-hw-08-gallery

--- hw-08-gallery/js/index.js
+++ hw-08-gallery/js/index.js
@@ -1,78 +1,120 @@
-// =============== <<<--- ЗАДАНИЕ --->>>  ===============
+// =============== <<<--- HOME TASK #8 - GALLERY SHOW --->>>  ===============
+{
+  /*
+  *   <li class="gallery__item">
+  *   <a
+  *     class="gallery__link"
+  *     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+  *   >
+  *     <img
+  *       class="gallery__image"
+  *       src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+  *       data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+  *       alt="Tulips"
+  *     />
+  *     <span class="gallery__icon">
+  *       <i class="material-icons">zoom_out_map</i>
+  *     </span>
+  *   </a>
+  * </li>
+  * 
+  * 
+  * Задание
+  * Создай галерею с возможностью просмотра полноразмерного изображения в модальном окне. Превью результата посмотри по ссылке.
+  * Необходимо динамически создать элементы галереи по указанному шаблону.
+  * При клике по элементу галереи должно открываться модальное окно с полноразмерным изображением. 
+  * Обязательно используй делегирование событий и слушай клики на элементе ul.gallery.
+  * Модальное окно должно закрываться по клику на кнопку button[data-action="close-modal"], по клику на div.overlay или по нажатию ESC.
+  */
+}
 
-const galleryItems = [
-  {
-    preview: "/img/preview-1.jpg",
-    fullview: "img/fullview-1.jpg",
-    alt: "alt text 1"
-  },
-  {
-    preview: "/img/preview-2.jpg",
-    fullview: "img/fullview-2.jpg",
-    alt: "alt text 2"
-  },
-  {
-    preview: "/img/preview-3.jpg",
-    fullview: "img/fullview-3.jpg",
-    alt: "alt text 3"
-  },
-  {
-    preview: "/img/preview-4.jpg",
-    fullview: "img/fullview-4.jpg",
-    alt: "alt text 4"
-  },
-  {
-    preview: "/img/preview-5.jpg",
-    fullview: "img/fullview-5.jpg",
-    alt: "alt text 5"
-  },
-  {
-    preview: "/img/preview-6.jpg",
-    fullview: "img/fullview-6.jpg",
-    alt: "alt text 6"
+import galleryShow from './gallery-items.js';
+
+// ********** IMPLEMENTATION OF THE STRUCTURE DOM ACCODANCE WITH THE TEMPLATE **********
+
+const galleryList = document.querySelector('.js-gallery');
+
+galleryShow.forEach(picture => {
+  function addGalleryImages( {preview, original, description} ) {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery_item');
+
+    const galleryImg = document.createElement('img');
+    galleryImg.classList.add('gallery_image');
+    galleryImg.setAttribute('src', preview);
+    galleryImg.setAttribute('data-source', original);
+    galleryImg.setAttribute('alt', description);
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery_link');
+    const atributeDataSource = galleryImg.getAttribute('data-source');
+    galleryLink.setAttribute('href', atributeDataSource);
+
+    const gallerySpan = document.createElement('span');
+    gallerySpan.classList.add('gallery_icon')
+
+    const galleryItalic = document.createElement('i');
+    galleryItalic.classList.add('material_icons');
+    galleryItalic.textContent = 'zoom_out_map';
+
+    gallerySpan.append(galleryItalic);
+    galleryLink.append(galleryImg);
+    galleryLink.append(gallerySpan);
+    galleryItem.append(galleryLink);
+    galleryList.append(galleryItem);
+
   }
-];
+  addGalleryImages(picture);
+
+});
 
 
-const refs = {
-  imageGallery: document.querySelector(".image-gallery"),
-  preview: document.querySelector(".preview"),
-  fullview: document.querySelector(".fullview"),
-};
+// ********** IMPLEMENTAION OF THE FUNCTIONALITY OF GALLERY_SHOW **********
 
-// console.log(refs.fullview);
-// refs.fullview.innerHTML = `<img src='${galleryItems[0].fullview}'/>`; // вставка через innerHTML
-// const firsImageInDocument = `<`
+const moddalLightbox =document.querySelector('.js-lightbox');
+const lightboxImage = document.querySelector('.js-lightbox_image');
+const lightboxButton = document.querySelector('buton[data-action="clse-lightbox"]');
+const backdropContent = document.querySelector('.js-lightbox-constent');
 
-refs.fullview.insertAdjacentHTML(
-  "afterbegin",
-  `<img src='${galleryItems[0].fullview}'/>`
-);
+galleryList.addEventListener('click', openModal);
+lightboxButton.addEventListener('click', closeModal);
+backdropContent.addEventListener('click', handleBackdrop);
 
+function openModal(e) {
+  e.preventDefault();
+  window.addEventListener('keyDown', handleKeyPress);
+  if (e.target === e.currentTarget) {
+    return;
+  }
+}
 
+moddalLightbox.classList.add('is-open');
 
-
-// galleryItems.reduce((acc, {preview, fullview, alt}) => {
-//   // const result = acc + `<li><img src="img/preview-1.jpg" data-fullview="img/fullview-1.jpg" alt="alt text 1"></li>`
-//   acc + refs.preview.innerHTML += `<li><img src="${preview}" data-fullview="${fullviwe}" alt="${alt}"></li>`;
-// }, '');
-
-
-const result = galleryItems
-.map(
-  ({preview, fullview, alt}) => 
-  `<li><img src="${preview}" data-fullview="${fullview}" alt="${alt}"></li>`
-)
-.join("");
+const atributeSrc = e.target.getAttribute('data-source');
+const atributeAlt = e.target.getAttribute('alt');
+lightboxImage.setAttribute('src', atributeSrc);
+lightboxImage.setAttribute('alt', atributeAlt);
 
 
-refs.preview.insertAdjacentHTML("beforeend", result);
- 
-// Вешаем слушателя на картинки
-const handlePreviewImage = event => {
-  console.log(event.target);
-  console.log(event.target.dataset.fullview);
-  refs.fullview.innerHTML = `<img src='${event.target.dataset.fullview}'/>`;
-};
+function closeModal () {
+  moddalLightbox.classList.remove('is-open');
+  window.removeEventListener('keydown', handleKeyPress);
+}
 
-refs.preview.addEventListener('click', handlePreviewImage);
+function handleBackdrop (e) {
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  closeModal();
+}
+
+function handleKeyPress (e) {
+  if (e.code !== 'Escape') {
+    return;
+  }
+  closeModal();
+}
+
+
+
+
