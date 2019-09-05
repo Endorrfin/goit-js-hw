d067ed4d1297ce94ea1475d978921e17235d3572
Vasyl Krupka <krupka.ua@gmail.com>
2019-09-05 18:24:35 +0300

    GoIT BootCamp#14 course JavaScript hw-08-gallery

--- hw-08-gallery/js/index.js
+++ hw-08-gallery/js/index.js
@@ -20,100 +20,60 @@
   * 
   * 
   * Задание
-  * Создай галерею с возможностью просмотра полноразмерного изображения в модальном окне. Превью результата посмотри по ссылке.
+  * Создай галерею с возможностью просмотра полноразмерного изображения в модальном окне.
   * Необходимо динамически создать элементы галереи по указанному шаблону.
   * При клике по элементу галереи должно открываться модальное окно с полноразмерным изображением. 
   * Обязательно используй делегирование событий и слушай клики на элементе ul.gallery.
-  * Модальное окно должно закрываться по клику на кнопку button[data-action="close-modal"], по клику на div.overlay или по нажатию ESC.
+  * Модальное окно должно закрываться по клику:
+  *   на кнопку button[data-action="close-modal"], 
+  *   по клику на div.overlay 
+  *   по нажатию ESC.
   */
 }
 
 import galleryShow from './gallery-items.js';
 
+
+
+
 // ********** IMPLEMENTATION OF THE STRUCTURE DOM ACCODANCE WITH THE TEMPLATE **********
 
-const galleryList = document.querySelector('.js-gallery');
+const gallery = document.querySelector('.js-gallery');
 
 galleryShow.forEach(picture => {
-  function addGalleryImages( {preview, original, description} ) {
-    const galleryItem = document.createElement('li');
-    galleryItem.classList.add('gallery_item');
+  function addGalleryImages({preview, original, description}) {
+    const li = document.createElement('li');
+    li.classList.add('gallery_item');
 
-    const galleryImg = document.createElement('img');
-    galleryImg.classList.add('gallery_image');
-    galleryImg.setAttribute('src', preview);
-    galleryImg.setAttribute('data-source', original);
-    galleryImg.setAttribute('alt', description);
+    const img = document.createElement('img');
+    img.classList.add('gallery_image');
+    img.setAttribute('src', preview);
+    img.setAttribute('data-source', original);
+    img.setAttribute('alt', description);
+ 
 
-    const galleryLink = document.createElement('a');
-    galleryLink.classList.add('gallery_link');
-    const atributeDataSource = galleryImg.getAttribute('data-source');
-    galleryLink.setAttribute('href', atributeDataSource);
+    const a = document.createElement('a');
+    a.classList.add('gallery_link');
+    const attributeDataSource = img.getAttribute('data-source');
+    a.setAttribute('href', attributeDataSource);
 
-    const gallerySpan = document.createElement('span');
-    gallerySpan.classList.add('gallery_icon')
+    const span = document.createElement('span');
+    span.classList.add('gallery_icon');
 
-    const galleryItalic = document.createElement('i');
-    galleryItalic.classList.add('material_icons');
-    galleryItalic.textContent = 'zoom_out_map';
+    const i = document.createElement('i');
+    i.classList.add('material-icons');
+    i.textContent = 'zoom_out_map';
 
-    gallerySpan.append(galleryItalic);
-    galleryLink.append(galleryImg);
-    galleryLink.append(gallerySpan);
-    galleryItem.append(galleryLink);
-    galleryList.append(galleryItem);
+    span.append(i);
+    a.append(img);
+    a.append(span);
+    li.append(a);
+    gallery.append(li);
 
   }
   addGalleryImages(picture);
 
 });
-
-
-// ********** IMPLEMENTAION OF THE FUNCTIONALITY OF GALLERY_SHOW **********
-
-const moddalLightbox =document.querySelector('.js-lightbox');
-const lightboxImage = document.querySelector('.js-lightbox_image');
-const lightboxButton = document.querySelector('buton[data-action="clse-lightbox"]');
-const backdropContent = document.querySelector('.js-lightbox-constent');
-
-galleryList.addEventListener('click', openModal);
-lightboxButton.addEventListener('click', closeModal);
-backdropContent.addEventListener('click', handleBackdrop);
-
-function openModal(e) {
-  e.preventDefault();
-  window.addEventListener('keyDown', handleKeyPress);
-  if (e.target === e.currentTarget) {
-    return;
-  }
-}
-
-moddalLightbox.classList.add('is-open');
-
-const atributeSrc = e.target.getAttribute('data-source');
-const atributeAlt = e.target.getAttribute('alt');
-lightboxImage.setAttribute('src', atributeSrc);
-lightboxImage.setAttribute('alt', atributeAlt);
-
-
-function closeModal () {
-  moddalLightbox.classList.remove('is-open');
-  window.removeEventListener('keydown', handleKeyPress);
-}
-
-function handleBackdrop (e) {
-  if (e.target === e.currentTarget) {
-    return;
-  }
-  closeModal();
-}
-
-function handleKeyPress (e) {
-  if (e.code !== 'Escape') {
-    return;
-  }
-  closeModal();
-}
 
 
 
